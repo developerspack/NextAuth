c34f1dd49f3c5200eb498403ab448d4fff7071aa
developerspack <developerspack23@gmail.com>
2023-06-26 15:03:30 +0300

    update

--- components/ProfileIcon.jsx
+++ components/ProfileIcon.jsx
@@ -9,7 +9,7 @@ export default function ProfileIcon() {
   if (status === "authenticated") {
     return (
       <div className="flex gap-5">
-        <Image
+        <img
           src={session.user.image}
           width={40}
           height={40}
